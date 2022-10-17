623a4c4c86119f534c0563ac6f0a564fe5cd0b95
Thyago Jesuino da Silva <82784831+ThyagoJesuino@users.noreply.github.com>
2022-10-17 18:48:51 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,11 +1,15 @@
 public class Usuario {
     public String nome;
+    public String senha;
+    public String recuperar;
     public String profissao;
     public boolean coord;
     
-    public Usuario(String nome, String profissao){
+    public Usuario(String nome,String senha,String recuperar,String profissao){
         this.nome = nome;
         this.profissao = profissao;
+        this.senha = senha;
+        this.recuperar = recuperar;
         if(profissao.equals("professor") || profissao.equals("coordenador") || profissao.equals("Professor") || profissao.equals("Coordenador")){
             coord = true;
         }
@@ -18,5 +22,8 @@ public class Usuario {
         System.out.println("Nome do usuário: " + this.nome);
         System.out.println("Profissão do usuário: " + this.profissao);
         System.out.println("Pode ser coordenador: " + (this.coord ? "Sim" : "Não"));
+    }
+    public void recuperarSenha(){
+        System.out.println("Senha do Usuario: " + this.senha);
     }
 }
