6a461d8adcbfa0924bdc2a20a6327c3fa2cca666
Thyago Jesuino da Silva <82784831+ThyagoJesuino@users.noreply.github.com>
2022-10-18 20:25:06 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -19,6 +19,7 @@ public class Usuario {
         System.out.println("Usuario adicionado com sucesso!");
     }
     public void printarInformacoesuser(){
+        //return "\nNome do usuário: " + this.nome + "\nProfissão do usuário: " + this.profissao + "\nPode ser coordenador: " + (this.coord ? "Sim" : "Não");
         System.out.println("Nome do usuário: " + this.nome);
         System.out.println("Profissão do usuário: " + this.profissao);
         System.out.println("Pode ser coordenador: " + (this.coord ? "Sim" : "Não"));
@@ -26,4 +27,8 @@ public class Usuario {
     public void recuperarSenha(){
         System.out.println("Senha do Usuario: " + this.senha);
     }
+    public static void main(String[] args) {
+        
+    }
 }
+  
